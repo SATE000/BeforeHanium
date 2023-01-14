34025b304863a1edcf7a04312b8ba88197e68356
SATE000 <lcw200620@gmail.com>
2023-01-14 23:36:21 +0900

    세번째 커밋 (23.01.14)

--- src/test/java/hello/hellospring/member/MemberServiceTest.java
+++ src/test/java/hello/hellospring/member/MemberServiceTest.java
@@ -1,19 +1,28 @@
 package hello.hellospring.member;
 
+import hello.hellospring.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;// = new MemberServiceImpl();
+
+    @BeforeEach //Test를 실행하기전 무조건 실행된다
+    public void beforeEach () {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     public void test_Join () {
-        Member member = new Member(1L, "TestMember", Grade.VIP);
-
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when
         memberService.join(member);
         Member findMember = memberService.fingMember(1L);
-
+        //then
         Assertions.assertThat(member).isEqualTo(findMember);
     }
 
