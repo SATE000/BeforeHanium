bf612f21b714c4620d2cd9dcbaf8bc9908ed02be
SATE000 <lcw200620@gmail.com>
2023-01-22 17:28:16 +0900

    네번째 커밋 (23.01.22)

--- src/main/java/hello/hellospring/AppConfig.java
+++ src/main/java/hello/hellospring/AppConfig.java
@@ -1,20 +1,39 @@
 package hello.hellospring;
 
+import hello.hellospring.discount.DiscountPolicy;
 import hello.hellospring.discount.FixDiscountPolicy;
+import hello.hellospring.discount.RateDiscountPolicy;
+import hello.hellospring.member.MemberRepository;
 import hello.hellospring.member.MemberService;
 import hello.hellospring.member.MemberServiceImpl;
 import hello.hellospring.member.MemoryMemberRepository;
 import hello.hellospring.order.OrderService;
 import hello.hellospring.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService () {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService () {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy () {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
